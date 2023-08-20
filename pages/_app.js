f39ba2b01aa7bb7e425a39aef9b75d0474ab23e2
Obinna Ukandu <obinnaukandu54@gmail.com>
2023-08-20 21:10:14 +0100

    modified  -app

--- pages/_app.js
+++ pages/_app.js
@@ -10,4 +10,4 @@ export default function App({ Component, session, ...pageProps }) {
       </FacepalContext>
     </SessionProvider>
   )
-}
+}                                                                                                                                
