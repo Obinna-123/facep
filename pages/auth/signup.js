79546b769e3bbaa89b45db8d46a1505e3d7f289f
Obinna Ukandu <obinnaukandu54@gmail.com>
2023-08-22 22:40:41 +0100

    github authentication

--- pages/auth/signup.js
+++ pages/auth/signup.js
@@ -1,32 +1,31 @@
-import { useFormik } from "formik";
-import * as yup from 'yup';
-import { authentication } from "@/settings/firebase.setting";
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { signIn,useSession } from "next-auth/react";
-import { useRouter } from "next/router";
+import { useFormik } from "formik"
+import * as yup from 'yup'
+import { signIn, useSession } from "next-auth/react"
+import { authentication } from "@/settings/firebase.setting"
+import { createUserWithEmailAndPassword } from "firebase/auth"
+import { useRouter } from "next/router"
 
-//validation rules
 const validationRules = yup.object().shape({
     email:yup.string().required('this field is compulsory'),
     password:yup.string()
     .required()
-    .min(8,'must be up to 8 character')
-    .max(36,'cannot be more than 36 characters')
-    .oneOf([yup.ref('passwordConfirmation'),null],'Your password must match')
+    .min(8,'must be up to 8 characters')
+    .max(36,'cannot be more than 36 charactters')
+    .oneOf([yup.ref('passwordConfirmation'),null],'your password must match')
 })
 
 export default function Signup() {
     const {data:session} = useSession();
-    const router = useRouter();
+    const router = useRouter()
 
     if(session) {
-        router.push('/feeds')
+        router.push("/feeds")
     }
 
-    const handleGoogleEmailPaswordCreateAccount = async (userEmail,userPassword) => {
+    const handleGoogleEmailPasswordCreateAccount = async (userEmail,userPassword) => {
         createUserWithEmailAndPassword(authentication,userEmail,userPassword)
         .then((user) => {
-            console.log(user)
+            console.log(user);
         })
         .catch((error) => console.error(error))
     }
@@ -34,11 +33,9 @@ export default function Signup() {
     const {handleBlur,handleSubmit,handleChange,errors,touched,values} = useFormik({
         initialValues:{email:'',password:'',passwordConfirmation:''},
         onSubmit: values => {
-            //create user account
-            handleGoogleEmailPaswordCreateAccount(values.email,values.password)
-        
+            handleGoogleEmailPasswordCreateAccount(values.email,values.password);
         },
-        validationSchema:validationRules //here too
+        validationSchema:validationRules
     });
 
     return (
@@ -48,29 +45,25 @@ export default function Signup() {
                 <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
                     <input 
                     id="email"
-                    type="email" 
+                    type="email"
                     value={values.email}
                     onChange={handleChange}
                     onBlur={handleBlur}
                     placeholder="Email address"
                     className="py-3 sm:py-5 px-2 border border-indigo-400 rounded-lg bg-white/60"
                     />
-                    {errors.email && touched.email 
-                    ? <span className="text-red-500">{errors.email}</span> 
-                    : null}
+                    {errors.email && touched.email ? (<span className=" text-red-500">{errors.email}</span>) : null}
 
                     <input 
                     id="password"
                     type="password" 
-                    value={values.password}
+                    value={values.pasword}
                     onChange={handleChange}
                     onBlur={handleBlur}
                     placeholder="Password"
                     className="py-3 sm:py-5 px-2 border border-indigo-400 rounded-lg bg-white/60"
                     />
-                    {errors.password && touched.password 
-                    ? <span className="text-red-500">{errors.password}</span> 
-                    : null}
+                    {errors.password && touched.password ? <span className=" text-red-500">{errors.password}</span> : null}
 
                     <input 
                     id="passwordConfirmation"
@@ -78,18 +71,18 @@ export default function Signup() {
                     value={values.passwordConfirmation}
                     onChange={handleChange}
                     onBlur={handleBlur}
-                    placeholder="confirm password"
+                    placeholder="Confirm Password"
                     className="py-3 sm:py-5 px-2 border border-indigo-400 rounded-lg bg-white/60"
                     />
                     <button type="submit" className="max-w-[160px] h-12 bg-indigo-800 rounded-lg text-white font-bold"
                     >Create Account</button>
                 </form>
         
-                <div className="w-full grid grid-cols-2 gap-3">
-                    <button 
-                    onClick={() => signIn('google')}
-                    className="w-full h-12 bg-green-600 rounded-lg text-white font-bold"
-                    >Google</button>
+                <div className="w-full grid grid-cols-3 gap-3">
+                    <button className="w-full h-12 bg-green-600 rounded-lg text-white font-bold"
+                    onClick={() => signIn("google")}>Google</button>
+                    <button className="w-full h-12 bg-gray-800 rounded-lg text-white font-bold"
+                    onClick={() => signIn("github")}>Github</button>
                     <button className="w-full h-12 bg-sky-600 rounded-lg text-white font-bold">Twitter</button>
                 </div>
             </div>
