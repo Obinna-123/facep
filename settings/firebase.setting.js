775ee740ab2301ce850cd5e1d2e959e99bac265f
Obinna Ukandu <obinnaukandu54@gmail.com>
2023-08-24 10:35:28 +0100

    corrected my firebase setting

--- settings/firebase.setting.js
+++ settings/firebase.setting.js
@@ -6,12 +6,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCsHE_KPrX8B6NmlX4oRo8A7NeW56fozxM",
-  authDomain: "facepal-c0e4e.firebaseapp.com",
-  projectId: "facepal-c0e4e",
-  storageBucket: "facepal-c0e4e.appspot.com",
-  messagingSenderId: "205455310577",
-  appId: "1:205455310577:web:b96fb3a94e43687ec23350"
+  apiKey: "AIzaSyDohr07exlc1gV9g1ICVA6KnTLTTvSKFm4",
+  authDomain: "facepal-de84f.firebaseapp.com",
+  projectId: "facepal-de84f",
+  storageBucket: "facepal-de84f.appspot.com",
+  messagingSenderId: "769520005324",
+  appId: "1:769520005324:web:3ba4c7d9cf3eb7c0215269"
 };
 
 // Initialize Firebase
