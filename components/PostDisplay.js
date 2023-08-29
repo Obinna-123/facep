1c06098b4431083f5cf0406f971bf7bc28128be9
Obinna Ukandu <obinnaukandu54@gmail.com>
2023-08-29 11:28:49 +0100

    modified and created update and delete function

--- components/PostDisplay.js
+++ components/PostDisplay.js
@@ -1,17 +1,63 @@
+import * as React from 'react';
 import Image from 'next/image';
 import { useSession } from 'next-auth/react';
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 import ChatBubbleOutlineRoundedIcon from '@mui/icons-material/ChatBubbleOutlineRounded';
-import ReplyOutlinedIcon from '@mui/icons-material/ReplyOutlined';
 import PublicIcon from '@mui/icons-material/Public';
-import ClearIcon from '@mui/icons-material/Clear';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import { hoursAgo } from '@/assets/hours-ago';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { Button,TextField } from '@mui/material';
+import CustomDialog from './CustomDialog';
+import { db } from '@/settings/firebase.setting';
+import { doc,deleteDoc,updateDoc, } from 'firebase/firestore';
 
-export default function PostDisplay({timePosted,body,postImage}) {
+export default function PostDisplay({ postID,timePosted,body,postImage}) {
     const {data:session} = useSession();
+    const [formInput,setFormInput] = React.useState(body);//FOR UPDATE POST FORM
+
+    //MENU CONTROL >>>> START
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => setAnchorEl(event.currentTarget);
+    const handleClose = () => setAnchorEl(null);
+    //MENU CONTROL >>>> END
+
+    //DIALOG CONTROL >>>> START
+    const [openDialog, setOpenDialog] = React.useState(false);
+    const handleClickOpenDialog = () => setOpenDialog(true);
+    const handleCloseDialog = () => setOpenDialog(false);
+    //DIALOG CONTROL >>>> END
+
+    //Update DIALOG CONTROL >>>> START
+    const [openDialogUpdate, setOpenDialogUpdate] = React.useState(false);
+    const handleClickOpenDialogUpdate = () => setOpenDialogUpdate(true);
+    const handleCloseDialogUpdate = () => setOpenDialogUpdate(false);
+    //Update DIALOG CONTROL >>>> END
+
+    //FUNCTION FOR DELETE POST
+    const handleDeletePost = async () => {
+        await deleteDoc(doc(db,'posts',postID))
+        .then(() => alert('post deleted'))
+        .catch(e => console.error(e))
+    }
+
+    //FUNCTION FOR UPDATE POST
+    const handleUpdatepost = async () => {
+        await updateDoc(doc(db,'posts',postID),{
+            body:formInput,
+            updatedAt: new Date().getTime(),
+        },
+        {
+            merge:true,
+        })
+        .then(() => alert('Post Updated Succesfully'))
+        .catch(error => console.error(error))
+    } 
 
     return (
+        <>
         <div className="border border-gray-100 bg-white rounded-md shadow-md py-4 mb-4">
             <ul className="flex justify-between px-4">
                 <li className="flex flex-row gap-1 items-center">
@@ -31,7 +77,8 @@ export default function PostDisplay({timePosted,body,postImage}) {
                 <li>
                     <div className="text-gray-700">
                         <button className='p-2 hover:bg-gray-200 rounded-full'>
-                            <MoreHorizIcon />
+                            <MoreHorizIcon
+                            onClick={handleClick} />
                         </button>
                     </div>
                 </li>
@@ -67,5 +114,60 @@ export default function PostDisplay({timePosted,body,postImage}) {
                 </button>
             </div>
         </div>  
+
+        <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+        >
+            <MenuItem onClick={handleClickOpenDialogUpdate}>Update</MenuItem>
+            <MenuItem onClick={handleClickOpenDialog}>Delete</MenuItem>
+        </Menu>
+
+        {/* DELETE DIALOG */}
+        <CustomDialog 
+        openProp={openDialog} 
+        handleCloseProp={handleCloseDialog} 
+        title='Delete post?'>
+            <p>Confirm post deletion</p>
+            <Button 
+            variant='outlined' 
+            color='error' 
+            onClick={handleDeletePost}>
+                Yes, delete
+            </Button>
+        </CustomDialog>
+
+        {/* UPDATE DIALOG */}
+        <CustomDialog 
+        openProp={openDialogUpdate} 
+        handleCloseProp={handleCloseDialogUpdate} 
+        title='Update post'>
+            <TextField
+            multiline={true}
+            className='w-full'
+            value={formInput}
+            onChange={(text) => setFormInput(text.target.value)}/>
+            
+            <Button 
+            variant='outlined' 
+            color='primary' 
+            style={{marginTop:8}}           
+            onClick={handleUpdatepost}
+            >
+                Update 
+            </Button>
+        </CustomDialog>
+        </>
     )
 }
