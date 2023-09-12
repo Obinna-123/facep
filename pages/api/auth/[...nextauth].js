2cfab4cd68eef85eda27d526940e597722fae4d0
Obinna Ukandu <obinnaukandu54@gmail.com>
2023-09-12 15:10:47 +0100

    key props addition

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github"
 import { cert } from "firebase-admin/app";
 import { FirestoreAdapter } from "@auth/firebase-adapter";
 
